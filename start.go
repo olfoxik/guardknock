9703035059faaa730a43fc379d299a12282e25d6
olfoxik <113599760+olfoxik@users.noreply.github.com>
2023-11-27 22:28:43 +0300

    Update start.go

--- start.go
+++ start.go
@@ -11,5 +11,19 @@ r.Use(middleware.Logger)
 r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 w.Write([]byte("welcome"))
 })
+
+
+router.Route("/url", func(r chi.Router) {
+    // Подключаем авторизацию
+    r.Use(middleware.BasicAuth("url-shortener", map[string]string{
+        // Передаем в middleware креды
+        "eee": "vvvv",
+        // Если у вас более одного пользователя,
+        // то можете добавить остальные пары по аналогии.
+    }))
+
+    r.Post("/", save.New(log, storage))
+})
+
 http.ListenAndServe(":3000", r)
 											}
