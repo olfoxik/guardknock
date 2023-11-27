8b125ba23c3d3c69e73ce3525c269a7fc05d1903
olfoxik <113599760+olfoxik@users.noreply.github.com>
2023-11-27 19:03:43 +0300

    Update start.go

--- start.go
+++ start.go
@@ -1,17 +1,15 @@
 package main
 # d12344
 import (
-		"net/http"
+"net/http"
+"github.com/go-chi/chi/v5"
+"github.com/go-chi/chi/v5/middleware")
 
-			"github.com/go-chi/chi/v5"
-				"github.com/go-chi/chi/v5/middleware"
-			)
-
-			func main() {
-					r := chi.NewRouter()
-						r.Use(middleware.Logger)
-							r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-										w.Write([]byte("welcome"))
-											})
-												http.ListenAndServe(":3000", r)
+func main() {
+r := chi.NewRouter()
+r.Use(middleware.Logger)
+r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+w.Write([]byte("welcome"))
+})
+http.ListenAndServe(":3000", r)
 											}
