b541b1b2df84f21304d7b0f035ac694cd609f405
root <root@2192403-zj29747.twc1.net>
2023-11-27 18:59:24 +0300

    [test]

---
+++ start.go
@@ -0,0 +1,17 @@
+package main
+# d12344
+import (
+		"net/http"
+
+			"github.com/go-chi/chi/v5"
+				"github.com/go-chi/chi/v5/middleware"
+			)
+
+			func main() {
+					r := chi.NewRouter()
+						r.Use(middleware.Logger)
+							r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+										w.Write([]byte("welcome"))
+											})
+												http.ListenAndServe(":3000", r)
+											}
