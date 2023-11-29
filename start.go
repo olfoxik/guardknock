c1e946d40790bcaec7dd8845f054cc2a92afde76
olfoxik <113599760+olfoxik@users.noreply.github.com>
2023-11-29 22:35:46 +0300

    Update start.go

--- start.go
+++ start.go
@@ -7,9 +7,21 @@ import (
 "github.com/BurntSushi/toml"
 "time")
 
-
+type Config struct {
+  Age int
+  Cats []string
+  Pi float64
+  Perfection []int
+  DOB time.Time
+}
 
 func main() {
+
+var conf Config
+if _, err := toml.DecodeFile("config.toml", &conf); err != nil {
+        // обработка ошибки.
+}
+	
 r := chi.NewRouter()
 r.Use(middleware.BasicAuth("url-shortener", map[string]string{
 "olfox": "A78GHKJG#",
