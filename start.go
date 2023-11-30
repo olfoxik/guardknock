f80057090fc9500f6e0ae30b343aaafbb8b6332d
olfoxik <113599760+olfoxik@users.noreply.github.com>
2023-11-30 22:35:05 +0300

    Update start.go

--- start.go
+++ start.go
@@ -4,53 +4,16 @@ import (
 "net/http"
 "github.com/go-chi/chi/v5"
 "github.com/go-chi/chi/v5/middleware"
-"github.com/BurntSushi/toml"
-"time"
-	"os"
-	"net"
 )
 
-type tomlConfig struct {
-    Title string
-    Owner struct {
-        Name string
-        Dob  time.Time
-    }
-    Database struct {
-        Server        string
-        Ports         []int
-        ConnectionMax uint
-        Enabled       bool
-    }
-    Servers map[string]ServerInfo
-    Clients struct {
-        Data  [][]interface{}
-        Hosts []string
-    }
-}
 
-type ServerInfo struct {
-    IP net.IP
-    DC string
-}
 
 func main() {
-
-  f, err := os.Open("config.toml")
-    if err != nil {
-        panic(err)
-    }
-    defer f.Close()
-    var config tomlConfig
-    if err := toml.NewDecoder(f).Decode(&config); err != nil {
-        panic(err)
-    }
 
 r := chi.NewRouter()
 r.Use(middleware.BasicAuth("url-shortener", map[string]string{
 "olfox": "A78GHKJG#",
-"olfox2": "tuxpux7",
-     }))
+"olfox2": "tuxpux7",    }))
 r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 w.Write([]byte("Welcome"))
 })
