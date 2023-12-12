84eee886cd9f86384f341dd594a4d60f230fe595
olfoxik <113599760+olfoxik@users.noreply.github.com>
2023-12-12 23:31:13 +0300

    Update start.go

--- start.go
+++ start.go
@@ -13,22 +13,18 @@ import (
 
 func main() {
 
-cfg, err := ini.Load("config.toml")
-hh := cfg.Section("users").KeysHash()
-fmt.Println("map:", hh)	
+cfg, err := ini.Load("config.ini")
+userpwd := cfg.Section("users").KeysHash()
+fmt.Println("map:", userpwd)	
 if err != nil {
 fmt.Printf("Fail to read file: %v", err)
-os.Exit(1)     }
+os.Exit(1)   }
 
-//userpwd :=  map[string]string{
-//"olfox": "A78GHKJG#",
-//"olfox2": "tuxpux7",    }
-	
 r := chi.NewRouter()
-r.Use(middleware.BasicAuth("url-shortener", hh ))
+r.Use(middleware.BasicAuth("url-shortener", userpwd ))
 	
 r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-w.Write([]byte("Welcome"))
+w.Write([]byte("Welcome GuardKnock v 1.0 "))
 })
 
 
