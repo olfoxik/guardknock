56e93dc820ecb5fe70cf047f9fd2bde55097f9ff
olfoxik <113599760+olfoxik@users.noreply.github.com>
2023-12-12 22:49:56 +0300

    Update start.go

--- start.go
+++ start.go
@@ -14,7 +14,7 @@ import (
 func main() {
 
 cfg, err := ini.Load("config.toml")
-hh := cfg.Section("").Key("app_mode").Value()
+hh := cfg.Section("users").KeysHash()
 if err != nil {
 fmt.Printf("Fail to read file: %v", err)
 os.Exit(1)     }
