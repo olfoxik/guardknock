05a2006df1a245b103a99d814305808f867125b1
olfoxik <113599760+olfoxik@users.noreply.github.com>
2023-12-12 23:06:39 +0300

    Update start.go

--- start.go
+++ start.go
@@ -20,16 +20,16 @@ if err != nil {
 fmt.Printf("Fail to read file: %v", err)
 os.Exit(1)     }
 
-userpwd :=  map[string]string{
-"olfox": "A78GHKJG#",
-"olfox2": "tuxpux7",    }
+//userpwd :=  map[string]string{
+//"olfox": "A78GHKJG#",
+//"olfox2": "tuxpux7",    }
 	
 r := chi.NewRouter()
-r.Use(middleware.BasicAuth("url-shortener", userpwd ))
+r.Use(middleware.BasicAuth("url-shortener", hh ))
 	
 r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-//w.Write([]byte("www") )
-w.Write(hh )
+w.Write([]byte("www") )
+
 })
 
  
