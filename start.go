1548a836e5cf92020097bee34743f2f21fe5c30a
olfoxik <113599760+olfoxik@users.noreply.github.com>
2023-12-12 21:42:40 +0300

    Update start.go

--- start.go
+++ start.go
@@ -4,11 +4,18 @@ import (
 "net/http"
 "github.com/go-chi/chi/v5"
 "github.com/go-chi/chi/v5/middleware"
+"gopkg.in/ini.v1"
+	
 )
 
 
 
 func main() {
+
+cfg, err := ini.Load("config.toml")
+if err != nil {
+fmt.Printf("Fail to read file: %v", err)
+os.Exit(1)     }
 
 userpwd :=  map[string]string{
 "olfox": "A78GHKJG#",
@@ -18,9 +25,11 @@ r := chi.NewRouter()
 r.Use(middleware.BasicAuth("url-shortener", userpwd ))
 	
 r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-w.Write([]byte("Welcome"))
+w.Write([]byte(cfg.Section("server").Key("protocol").In("http", []string{"http", "https"}) ))
 })
 
+ 
 
+	
 http.ListenAndServe(":3002", r)
 											}
