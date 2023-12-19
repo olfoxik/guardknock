31fee4a87eaeb38450b35f93d9173d42b987ce73
olfoxik <113599760+olfoxik@users.noreply.github.com>
2023-12-19 21:40:55 +0300

    Update start.go

--- start.go
+++ start.go
@@ -16,7 +16,8 @@ func main() {
 cfg, err := ini.Load("config.ini")
 userpwd := cfg.Section("users").KeysHash()
 fmt.Println("map:", userpwd)
-url := cfg.Section("Main").Key("url").String()
+knockstr := cfg.Section("activstr").Key("knock").String()
+	
 if err != nil {
 fmt.Printf("Fail to read file: %v", err)
 os.Exit(1)   }
@@ -25,7 +26,7 @@ r := chi.NewRouter()
 r.Use(middleware.BasicAuth("url-shortener", userpwd ))
 	
 r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-w.Write([]byte("Welcome GuardKnock v 1.0 "))
+w.Write([]byte(knockstr))
 })
 
 
